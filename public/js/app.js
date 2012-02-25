747489c5bbcb69d2d68fd70f416b2e191238a546
NotoriousRGB <info@notoriousrgb.com>
2012-02-25 21:50:43 +1100

    Reverted back to flexslider

--- public/js/app.js
+++ public/js/app.js
@@ -1,9 +1,7 @@
 $(document).ready(function() {
 
-    
 
     $('#centeredmenu ul li').has('ul').hover(function(){
-
       var menuState = 'closed';
 
       if ( menuState == 'closed') {
@@ -11,35 +9,75 @@ $(document).ready(function() {
         var menuState = 'open';
       };
 
-
-      
-     
     }, function(e){
 
-      if ( $("#sequence").css("width") > "480px") {
+      if ( $("#navigation").css("width") > "480px") {
         $(this).find('ul').slideUp(200);
       } else {
         $(this).find('ul').hide();
       }
 
-      
     });
+
+    
+
+
+
+
 
     $('.flexslider').flexslider({
     	
     	animation: "slide",
+      slideDirection: "horizontal",
     	directionNav: false,
-			controlNav: true,
+			controlNav: false,
+      keyboardNav: true,
 			slideshowSpeed: 7000,          
-			animationDuration: 600,
+			animationDuration: 700,
 			controlsContainer: ".flex-container",
-      start: function(){
-        $('#slider h3').hide().fadeIn(2000);
+      start: function(slider){
+        $('.slides div.text-anim-1').css({ marginTop: '90px'});
       },
-      before: function(){
-        $('#slider h3').hide().fadeIn(2000);
+      before: function(slider){       
+        $('.slides div.img-anim-1').stop().css({ opacity: '0' }).delay(700).animate({
+          opacity: '1'
+        }, 1200);
+
+        $('.slides div.text-anim-1').stop().css({ opacity: '0' }).delay(750).animate({   
+          opacity: '1'
+        }, 1200);
       },
+      after: function(slider){
+        
+
+
+
+      }
     });
+
+   // var options = {
+   //      nextButton: false,
+   //      prevButton: false,        
+   //      autoPlayDelay: 3000,            
+   //      pauseOnHover: false,        
+   //      animateStartingFrameIn: true,         
+   //      delayDuringOutInTransitions: 750,
+   //      keysNavigate: true,               
+   //      touchEnabled: false,
+   //      startingFrameID: 1,         
+   //      fallbackTheme: {              
+   //        speed: 500
+   //      }
+   //    };
+
+   //    var sequence = $("#sequence").sequence(options).data("sequence");
+
+
+
+
+
+
+
 
    $('#portfolio img').hover(function(){
       $(this).animate({ opacity: '.4'}, 200);
